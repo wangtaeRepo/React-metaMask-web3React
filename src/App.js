5c5cedede674e16ccab757deaa99f820bdd13c3f
wangtae0320 <85270112+wangtae0320@users.noreply.github.com>
2022-01-12 19:36:21 +0900

    코드 수정 및 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import logo from './logo.svg';
-import './cover.css';
-import{InjectedConnector} from "@web3-react/injected-connector";
-import{useWeb3React} from "@web3-react/core";
+import './cover.css'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { useWeb3React } from '@web3-react/core'
 
 //contract MyNFT is ERC721,Ownable{
 
 function App() {
+  // 지원되는 네트워크 주입
   const injected = new InjectedConnector({
-    supportedChainIds:[1,3,4,5,42] //hardhat ropstan : 4,3
-
+    supportedChainIds: [1, 3, 4, 5, 42], //hardhat ropstan : 4,3
   })
-  const { active, account, library, connector, activate, deactivate } =
-    useWeb3React();
+  // const { active, account, library, connector, activate, deactivate } =
+  const { active, account, activate, deactivate } = useWeb3React()
   async function connect() {
     try {
-      await activate(injected);
+      await activate(injected)
     } catch (ex) {
-      console.log(ex);
+      console.log(ex)
     }
   }
   async function disconnect() {
     try {
-      deactivate();
+      deactivate()
     } catch (ex) {
-      console.log(ex);
+      console.log(ex)
     }
   }
   return (
     <>
-      <br></br><br></br><br></br><br></br><br></br>
-      <h1>NFT Raffle</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1>MetaMask 연결하기</h1>
       <div className="flex flex-col items-center justify-center">
         <button
           onClick={connect}
@@ -37,7 +40,10 @@ function App() {
         >
           Connect to MetaMask
         </button>
-        <br></br><br></br><br></br><br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         {active ? (
           <span>
             Connected with <b>{account}</b>
@@ -45,17 +51,23 @@ function App() {
         ) : (
           <span>Not connected</span>
         )}
-        <br></br><br></br><br></br><br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <button
           onClick={disconnect}
           className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800"
         >
           Disconnect
         </button>
-        <br></br><br></br><br></br><br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Web3Provider } from '@ethersproject/providers';
-import { Web3ReactProvider } from '@web3-react/core';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Web3Provider } from '@ethersproject/providers'
+import { Web3ReactProvider } from '@web3-react/core'
 
-import reportWebVitals from './reportWebVitals';
-function getLibrary(provider){
-  const library=new Web3Provider(provider);
-  library.pollingInterval=12000; //
-  return library;
+import reportWebVitals from './reportWebVitals'
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000 //
+  return library
 }
 ReactDOM.render(
   <React.StrictMode>
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <App />  
+    {/* Component 내에서 사용할 수 있도록 Provider 감싸기 */}
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
     </Web3ReactProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
